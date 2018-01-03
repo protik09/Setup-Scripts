dafc9ed25737115a6a38422635a155ac9316976b
protik09 <protik09@gmail.com>
2018-01-03 16:15:58 +0530

    Modified tools setup to include browser, text editors, etc.

--- linux_tools_n_themes.sh
+++ linux_tools_n_themes.sh
@@ -17,12 +17,19 @@ sudo add-apt-repository -y ppa:noobslab/icons
 echo "Now we'll add some useful widget and tool repositories...."
 sudo add-apt-repository -y ppa:atareao/atareao
 sudo add-apt-repository -y ppa:alexeftimie/ppa
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - # Add chrome keys
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' # Add chrome repo
+
+echo "Lets add the nice text editor repositories...."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list # Sublime Text 3 keys and repo address
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' # vscode keys and repo address
 
 echo "Let's get updates and install all this stuff..."
 sudo apt update
 sudo apt -y full-upgrade
-
-# Add proper SSH keys to your
 
 # Install Themes
 
@@ -34,16 +41,14 @@ sudo apt install -y pop-theme
 sudo apt install -y adapta-gtk-theme
 sudo apt install -y vertex-theme
 sudo apt install -y arc-theme
-sudo apt install -y windos-10-themes
-sudo apt install -y victory-icon-theme
 
 # Install Tools
 
 echo "Installing Tools...."
 sudo apt install -y wget
 sudo apt install -y git
-sudo apt install -y curl
 sudo apt install -y clang
+sudo apt install -y html-xml-utils
 sudo apt install -y openssh-server
 sudo apt install -y openssh
 sudo apt install -y flake8 autopep8
@@ -52,11 +57,17 @@ sudo apt install -y variety variety-slideshow
 sudo apt install -y redshift redshift-gtk
 sudo apt install -y indicator-multiload
 sudo apt install -y python-pip
+sudo apt install -y google-chrome-stable
+sudo apt install -y sublime-text
+sudo apt install -y code
+
+# Delete useless programs
+sudo apt-get remove --purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+sudo apt autoremove
 
 echo "Downloading Manual Install Packages"
-# atom_down = curl -s https://api.github.com/repos/atom/atom/releases | grep browser_download_url | grep '64[.]deb' | head -n 1 | cut -d '"' -f 4
-# wget $atom_down
-# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-# sudo apt update
-# sudo apt install sublime-text
+echo "Installing WPS Office"
+wget -O ~/Downloads/wps-office_a21.deb http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb #change this if it ever updates
+sudo dpkg -i ~/Downloads/wps-office_*.deb
