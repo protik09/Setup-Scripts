89b75210db975cb504b9a57d3c0902934751a7cf
Protik Banerji <protik09@users.noreply.github.com>
2019-04-17 11:54:02 +0530

    April 17th Update to git repository list

--- download_git_repos.sh
+++ download_git_repos.sh
@@ -3,13 +3,11 @@
 gitserver_ip='10.10.0.26'
 fsw_repo='/axiom/flight-software/depot'
 
-git clone $USER@$gitserver_ip:$fsw_repo/LfswTest.git
-cd LfswTest
-/bin/bash git_init_all.sh
-/bin/bash git_update_all.sh
-
 cd ~/Projects
+git clone $USER@$gitserver_ip:$fsw_repo/CUnit.git
 git clone $USER@$gitserver_ip:$fsw_repo/PrRover.git
 git clone $USER@$gitserver_ip:$fsw_repo/AtmelTraining.git
-git clone $USER@$gitserver_ip:$fsw_repo/LfswInterfaceSimulator.git
+git clone $USER@$gitserver_ip:$fsw_repo/LfswBuildScripts.git
 git clone $USER@$gitserver_ip:$fsw_repo/FlightSoftwareDocuments.git
+git clone $USER@$gitserver_ip:/axiom/flight-dynamics/depot/modular.git
+git clone $USER@$gitserver_ip:/axiom/lander-pcoc/depot/panguWrapper.git
