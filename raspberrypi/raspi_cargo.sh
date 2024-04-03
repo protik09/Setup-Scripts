557a07b22bda3b0a5dedffecf23a797793ec451d
Protik Banerji <protik09@users.noreply.github.com>
2024-04-03 23:59:44 +0200

    Error handling and install check added to rust apps installer

--- raspberrypi/raspi_cargo.sh
+++ raspberrypi/raspi_cargo.sh
@@ -1,2 +1,16 @@
-cargo install tealdeer
-cargo install just
+#!/usr/bin/env bash
+
+# Add manual skips if a command fails in the following script
+set -e
+
+# Install rust apps
+# Check if tealdeer already exists in rustup
+if ! command -v tldr &> /dev/null; then
+    echo "Installing tealdeer"
+    cargo install tealdeer
+fi
+# Check if just app already exists
+if ! command -v just &> /dev/null; then
+    echo "Installing just"
+    cargo install just
+fi
