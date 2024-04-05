14e1bc90b93fdd9b4a38060ef497e0e6e4c66166
Protik Banerji <protik09@users.noreply.github.com>
2024-04-05 03:58:10 +0200

    Use binstall for cargo installs going forward

--- raspberrypi/raspi_cargo.sh
+++ raspberrypi/raspi_cargo.sh
@@ -5,12 +5,17 @@ set -e
 
 # Install rust apps
 # Check if tealdeer already exists in rustup
+if ! command -v "cargo-binstall" &> /dev/null; then
+    echo "Installing binstall"
+    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+fi
+# Check if tealdeer already exists in rustup
 if ! command -v tldr &> /dev/null; then
     echo "Installing tealdeer"
-    cargo install tealdeer
+    cargo binstall tealdeer
 fi
 # Check if just app already exists
 if ! command -v just &> /dev/null; then
     echo "Installing just"
-    cargo install just
+    cargo binstall just
 fi
