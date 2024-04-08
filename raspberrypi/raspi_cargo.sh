a7ccea99e547a4eaca4fc975e1fbde566ffc27f5
Protik Banerji <protik09@users.noreply.github.com>
2024-04-08 23:31:49 +0200

    Add feedback to cargo install file if program already installed

--- raspberrypi/raspi_cargo.sh
+++ raspberrypi/raspi_cargo.sh
@@ -8,6 +8,8 @@ set -e
 if ! command -v "cargo-binstall" &> /dev/null; then
     echo "Installing binstall"
     curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+else
+    echo "Binstall already installed."
 fi
 # Check if tealdeer already exists in rustup
 if ! command -v bat &> /dev/null; then
@@ -23,14 +25,20 @@ if ! command -v bat &> /dev/null; then
             echo "$alias_def" >> ~/.bash_aliases
         fi
     done
+else
+    echo "Bat already installed."
 fi
 # Check if tealdeer already exists in rustup
 if ! command -v tldr &> /dev/null; then
     echo "Installing tealdeer"
     cargo binstall tealdeer -y
+else
+    echo "Tealdeer already installed."
 fi
 # Check if just app already exists
 if ! command -v just &> /dev/null; then
     echo "Installing just"
     cargo binstall just -y
+else
+    echo "Just already installed."
 fi
